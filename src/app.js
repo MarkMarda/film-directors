12c43ac7ebbaae6a76c4adc1a93fad191d8aaf48
Marco A. R. del A <marda_da-capo@hotmail.com>
2023-02-10 15:33:27 -0600

    file database configured; dependencies installed: pg, pg-hstore, sequelize and  uiid

--- src/app.js
+++ src/app.js
@@ -13,7 +13,8 @@ app.get("/", (req, res) => {
   res.status(200).json({
     message: "OK!",
     directors: `localhost: ${config.port}/api/v1/directors`
-  })
+  });
+  
 });
 
 app.listen(config.port, () => {
